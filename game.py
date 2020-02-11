69836231fd5c05e288bd76e5e128b67c75785057
Cerv0020 <60798989+Cerv0020@users.noreply.github.com>
2020-02-11 14:45:07 +0000

    Update game.py

--- game.py
+++ game.py
@@ -3,50 +3,24 @@ import sys
 import pygame
 import random
 
-numeroJogadoresCheck = True
-
-#Verificação de Inserção de Número de Jogadores (2-4)
-while(numeroJogadoresCheck):
-
-    numeroJogadores = input("Número de Jogadores: ")
-    numeroJogadores = int(numeroJogadores)
-
-    if (numeroJogadores == 1):
-        print("Este jogo não se pode jogar sozinho :c")
-    else:
-        if (numeroJogadores == 2):
-            numeroJogadoresCheck = False
+#Número de Jogadores
+while(True):
+    try:
+        numeroJogadores = int(input("Número de Jogadores: "))
+        if numeroJogadores >= 2 and numeroJogadores <= 4:
+            break
         else:
-            if (numeroJogadores == 3):
-                numeroJogadoresCheck = False
-            else:
-                if (numeroJogadores == 4):
-                    numeroJogadoresCheck = False
-                else:
-                    print("O número de jogadores tem que estar compreendido entre 2 e 4")
+            print("Número de Jogadores tem de estar compreendido entre 2 e 4")
+    except:
+        print("Insere apenas números inteiros.")
 
-#Nomes dos Jogadores
-nomeJogador1 = "Por Definir"
-nomeJogador2 = "Por Definir"
-nomeJogador3 = "Por Definir"
-nomeJogador4 = "Por Definir"
+#Nomes dos Jogadores 
+nomesJogadores = []
+for i in range(numeroJogadores):
+    nome = input("Nome do Jogador {0}: ".format(i + 1))
+    nomesJogadores.append(nome)
 
-if numeroJogadores == 2:
-    nomeJogador1 = input("Nome do jogador 1: ")
-    nomeJogador2 = input("Nome do jogador 2: ")
-
-if numeroJogadores == 3:
-    nomeJogador1 = input("Nome do jogador 1: ")
-    nomeJogador2 = input("Nome do jogador 2: ")
-    nomeJogador3 = input("Nome do jogador 3: ")
-
-if numeroJogadores == 4:
-    nomeJogador1 = input("Nome do jogador 1: ")
-    nomeJogador2 = input("Nome do jogador 2: ")
-    nomeJogador3 = input("Nome do jogador 3: ")
-    nomeJogador4 = input("Nome do jogador 4: ")
-
-#Escolha do Jogador que escolhe a palavra
+#Jogador da Palavra
 randomizador = random.randint(1, numeroJogadores)
 
 if randomizador == 1:
