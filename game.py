f8480ce5b76f1fa8ae9f0c567a041ebb290d51e2
Cerv0020 <60798989+Cerv0020@users.noreply.github.com>
2022-10-20 01:43:10 +0100

    Update game.py

--- game.py
+++ game.py
@@ -25,4 +25,15 @@ for i in range(numeroJogadores):
 randomizador = random.randint(1, numeroJogadores)
 print("Nome do jogador que irá escolher a palavra :" + nomesJogadores[randomizador - 1])
 palavra = getpass.getpass('Palavra: ')
-print(palavra)
+
+#Adivinha a palavra
+while(True):
+    try:
+        letra = input("Letra: ")
+        if letra.isalpha():
+            print("É letra")
+            break   
+        else:
+            print("Só letras podem ser introduzidas")
+    except:
+        print()
